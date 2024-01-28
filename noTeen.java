037127d6093e55ae5d2544b62a2048d3041a65cb
Deeksha313 <deekshabalakrishnan41593@gmail.com>
2024-01-28 16:23:25 +0530

    Assignment 2b

---
+++ noTeen.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class noTeen{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the three Numbers(a,b,c):");
+        int a,b,c;
+        a=sc.nextInt();
+        b=sc.nextInt();
+        c=sc.nextInt();
+        int x=noTeenSum(a,b,c);
+        System.out.println("Sum="+x);
+    }
+    
+    public static int noTeenSum(int a, int b,int c){
+        return fixTeen(a)+fixTeen(b)+fixTeen(c);
+    }
+    
+    public static int fixTeen(int n){
+        if((n>=13 && n<=19)&& !(n==15||n==16)) //helper method
+            return 0;
+        else
+            return n;
+    }
+}
